d44947f885f6006e8a812266acc8c8cb26b3fdea
Jefferson Belardo <iamjeffersonbelardo@gmail.com>
2025-04-09 22:46:37 +0800

    Added back to top button

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const toggleButton = document.querySelector(
-    '[data-collapse-toggle="navbar-default"]'
-  );
-  const navbar = document.getElementById("navbar-default");
+    const toggleButton = document.querySelector(
+        '[data-collapse-toggle="navbar-default"]'
+    );
+    const navbar = document.getElementById("navbar-default");
 
-  toggleButton.addEventListener("click", () => {
-    navbar.classList.toggle("hidden");
-  });
+    toggleButton.addEventListener("click", () => {
+        navbar.classList.toggle("hidden");
+    });
 });
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
